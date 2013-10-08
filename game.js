729ba2eceaf62ba277d7f70b92abe491dfefa920
Jordan Morano <jordan.morano@gmail.com>
2013-10-08 15:20:44 -0400

    redo

--- game.js
+++ game.js
@@ -3,13 +3,11 @@
 var board = function() {
 	var end = "<td id='boundary'>br</td>";
 
-	if (window.screen.width < 480){
-		if (window.orientation == 0 || window.orientation == 180) {
-			for (var i = 0; i < 23; i++) {
+	for (var i = 0; i < 23; i++) {
 				document.writeln("<tr>")
 					document.writeln(end)
 					for (var j = 0; j < (10); j++) {
-					document.writeln("<td id='b" + i+j +"'><span>" + i + j + "</span></td>");
+					document.writeln("<td><span>" + i + j + "</span></td>");
 					} 
 					document.writeln(end)
 				document.writeln("</tr>")
@@ -18,69 +16,18 @@ var board = function() {
 				for (var j = 0; j < (12); j++) {
 					document.writeln(end);
 				}
-		} else { //landscape here 
-			for (var i = 0; i < 10; i++) {
-				document.writeln("<tr>")
-					document.writeln(end)
-					for (var j = 0; j < (23); j++) {
-					document.writeln("<td id='b"+ i+j +"'><span>" + i + j + "</span></td>");
-					} 
-					document.writeln(end)
-				document.writeln("</tr>")
-			}
-			document.writeln("<tr>")
-				for (var j = 0; j < (25); j++) {
-					document.writeln(end);
-					}
-		}
-	} else { // desktop version
-		for (var i = 0; i < 23; i++) {
-				document.writeln("<tr>")
-					document.writeln(end)
-					for (var j = 0; j < (10); j++) {
-					document.writeln("<td id='b" + i+j +"'><span>" + i + j + "</span></td>");
-					} 
-					document.writeln(end)
-				document.writeln("</tr>")
-			}
-			document.writeln("<tr>")
-				for (var j = 0; j < (12); j++) {
-					document.writeln(end);
-				}
-
-	}
-
-var cw = document.getElementById('a[id^=b]').width();
-document.getElementById('a[id^=b]').css({'height':cw + 'px'});
 };
-
-
-
-// listen for orientation changes
-
-var windowChange = function(){
-	window.addEventListener('orientationchange', function(){
-		alert('Window orientation has changed')
-	}, false);
-}
-
-
-// remove last column
-
-
-
-//use a make function that adds id values to target td in order to make a shape
 
 // use arrays for each shape 
 
-var shapes = [
-	[20,21,22,23], // line
-	[21,30,31,41], //t-shape
-	[20,21,30,31], //square
-	[21,22,30,31], // s shape
-	[20,21,31,32], // z shape
-	[20,30,31,32], // j shape
-	[22,30,31,32] // l shape
+var shapeFormulas = [
+	[[0,0],[1,0],[2,0],[3,0]], // line
+	[[0,0],[1,0],[2,0],[1,1]], //t-shape
+	[[0,0],[1,0],[0,1],[1,1]], //square
+	[[0,0],[1,0],[1,1],[2,1]], // s shape
+	[[0,1],[1,1],[1,1],[2,0]], // z shape
+	[[0,0],[1,0],[2,0],[2,1]], // j shape
+	[[0,1],[1,1],[2,1],[2,0]] // l shape
 ];
 
 var shapeColor = [
